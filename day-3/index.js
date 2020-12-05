d224e0f1748fc8eb48d5d5ae4f0e640af88e3892
Vince Falconi <vince.falconi@gmail.com>
2020-12-05 00:18:15 -0600

    solve day 3

---
+++ day-3/index.js
@@ -0,0 +1,48 @@
+const input = require('./input');
+const countTrees = (treeMap, slope) => {
+	const calcX = (xLimit, currentPos, inc) => {
+		const newPos = currentPos + inc;
+		if (newPos >= xLimit) return newPos - xLimit;
+		return newPos;
+	}
+	const patternWidth = treeMap[0].length;
+	const finishY = treeMap.length + 1;
+	const position = {
+		x: 0,
+		y: 0,
+	}
+	let treeCount = 0;
+
+	while (position.y < finishY) {
+		position.x = calcX(patternWidth, position.x, slope.right);
+		position.y += slope.down;
+		if (position.y < treeMap.length) {
+			let space = treeMap[position.y][position.x];
+			if (space === '#') treeCount++;
+		}
+	}
+
+	return treeCount;
+}
+
+const a = (mapInput) => {
+	const slope = {
+		right: 3,
+		down: 1,
+	}
+	return countTrees(mapInput, slope);
+};
+
+const b = (mapInput) => {
+	const slopes = [
+    { right: 1, down: 1 },
+    { right: 3, down: 1 },
+    { right: 5, down: 1 },
+    { right: 7, down: 1 },
+    { right: 1, down: 2 },
+	];
+	const counts = slopes.map(slope => countTrees(mapInput, slope));
+	return counts.reduce((acc, curr) => acc * curr);
+}
+
+console.log(a(input), b(input));
