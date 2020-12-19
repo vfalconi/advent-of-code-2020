94a069607cf97e7b3a8c39ed45457d1d4a1f25c6
Vince Falconi <vince.falconi@gmail.com>
2020-12-19 17:05:44 -0600

    solve day 8

---
+++ day-8/index.js
@@ -0,0 +1,69 @@
+const input = require('./input');
+
+const deepCopy = (arr) => {
+	return arr.map(item => {
+		return {...item};
+	});
+};
+
+const runInstructions = (input) => {
+	const instructions = deepCopy(input);
+	let accumulator = 0;
+	let i = 0;
+	let infiniteLoop = false;
+
+	while (true) {
+		const step = instructions[i];
+
+		if (i === instructions.length) {
+			return {
+				accumulator,
+				infiniteLoop: false,
+			}
+		}
+
+		if (step.executed === true) {
+			return {
+				accumulator,
+				infiniteLoop: true,
+			}
+		}
+
+		step.executed = true;
+
+		if (step.operation === 'acc') {
+			accumulator += step.argument;
+		}
+
+		if (step.operation === 'jmp') {
+			i += step.argument;
+		} else {
+			i++;
+		}
+	}
+
+}
+
+const a = (input) => {
+	const walk = runInstructions(input);
+	return walk.accumulator;
+};
+
+const b = (input) => {
+	const toggleOperation = (operation) => {
+		if (operation === 'nop') return 'jmp';
+		if (operation === 'jmp') return 'nop';
+	};
+	const instructions = deepCopy(input);
+
+	for (let i = 0; i < instructions.length; i++) {
+		if (instructions[i].operation === 'nop' || instructions[i].operation === 'jmp') {
+			const testInstructions = deepCopy(instructions);
+			testInstructions[i].operation = toggleOperation(testInstructions[i].operation);
+			const walk = runInstructions(testInstructions);
+			if (walk.infiniteLoop === false) return walk.accumulator;
+		}
+	}
+};
+
+console.log(a(input), b(input));
